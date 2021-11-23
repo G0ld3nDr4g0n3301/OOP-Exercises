82b31f0b0f5c56285410e9e50eae028fff8df225
G0ld3nDr4gon3301 <nuchtonibudushe@mail.ru>
2021-11-23 16:13:56 +0300

    Added solution for exercise N4(simple version).

--- №5/cache.py
+++ №5/cache.py
@@ -1,33 +1,15 @@
 def cache(func): # Ещё не доделал.Залью остальное вместе с ЭТИМ в репозиторий.
 	d = {} 
 	def wrap(*args,**kwargs):
-		print(args,kwargs.items(),kwargs)
-		if len(args) == 1:
-			if args[0] in d.keys():
-				return d[args[0]]
+		if len(args) == 1 and len(kwargs.keys()) == 0:
+			if str(args[0]) in d.keys():
+				print('Loading cache...')
+				return d[str(args[0])]
 			else:
-				d.fromkeys(args[0],func(args[0]))
-				return d[args[0]]
-
-		elif len(kwargs.keys()) == 1:
-			if kwargs.items() in d.keys():
-				return d[kwargs.items()]
-			else:
-				d.formkeys(kwargs.items(),func(**kwargs))
-				return d[kwargs.items()]
-
+				d[str(args[0])] = func(args[0])
+				return d[str(args[0])]
 		else:
 			return func(*args,**kwargs)
-
-		name = f'{args,kwargs}'.format()
-		print(name)
-
-		if name in d.keys():
-			return d[name]
-
-		else:
-			d.fromkeys(name, func(*args,**kwargs))
-			return result
 
 	return wrap
 
@@ -37,4 +19,16 @@ def factorial(n):
 	return n * factorial(n-1) if n else 1
 
 
+# Можно создать строку kwstring = ''.join(str([kwargs.items()[i][1]) for i in range(len(kwargs.keys()))])
+# Или можно создавать списки внутри списков,и в них хранить все аргументы...
+# Ничего не придумывается... Сделаю пока простой вариант.
+
+print(factorial(12))
+
+print(factorial(10))
+
+print(factorial(7))
+
 print(factorial(5))
+
+print(factorial(1))
